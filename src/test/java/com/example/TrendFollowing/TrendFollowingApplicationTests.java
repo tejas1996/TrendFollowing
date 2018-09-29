8dcb4daff6ce729abc4584dbd0575ce09967be33
tejas1996 <tejas.dond@ves.ac.in>
2018-09-29 22:35:45 +0530

    Initial commit

---
+++ src/test/java/com/example/TrendFollowing/TrendFollowingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.TrendFollowing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrendFollowingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
